70289aa7194c19115424e370a1eeb72c8743164e
nimatuladhima <92084080+nimatuladhima@users.noreply.github.com>
2022-03-14 13:54:56 +0700

    Add files via upload

---
+++ 016_Ni'matul 'Adhima_JavaScript/js/script.js
@@ -0,0 +1,11 @@
+//memunculkan pop up button
+alert("SELAMAT DATANG DI WEBSITE JURUSAN TEKNIK INFORMATIKA");
+
+//memanipulasi teks
+document.getElementById("prodi.ti").innerHTML = "Program studi Teknik Informatika adalah prodi yang akan mempelajari berbagai prinsip terkait ilmu komputer mulai dari proses perancangan, pengembangan, pengujian, hingga evaluasi sistem operasi perangkat lunak. Selama kuliah kamu akan banyak mengkaji pemrograman dan komputasi, serta dibekali dengan keterampilan merancang perangkat lunak."
+
+document.getElementById("prodi_pti").innerHTML = "Program studi Pendidikan Teknologi Informasi merupakan program studi yang bertujuan untuk mempersiapkan seorang inovator pendidikan yang tidak hanya berkompeten dalam merancang suatu inovasi pembelajaran, namun juga terampil dalam memanfaatkan perkembangan teknologi informasi untuk tercapainya tujuan pembelajaran atau kompetensi yang diharapkan dari peserta didik."
+
+document.getElementById("prodi_si").innerHTML = "Program studi Sistem Informasi adalah bidang keilmuan yang menggabungkan ilmu komputer dengan bisnis dan manajemen. Di prodi ini kamu akan belajar gimana mengidentifikasi kebutuhan dan proses bisnis perusahaan berdasarkan data-data yang dimiliki perusahan, kemudian merancang sistem yang sesuai dengan kebutuhan perusahaan."
+
+document.getElementById("prodi_ti").innerHTML = "Program studi Manajemen Informatika adalah sebuah program studi di perguruan tinggi yang mengajarkan mahasiswanya tentang penggunaan teknologi untuk memudahkan kegiatan bisnis. Mahasiswa Jurusan Manajemen Informatika belajar tentang cara menganalisis, merancang, mengembangkan, hingga mengelola teknologi informasi."
